7aa343406445e4308f99b7b66d9e8f32385cf59f
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2024-01-24 21:10:06 +0000

    swiftBook HM Light

--- swiftBook HM1 (2)/ViewController.swift
+++ swiftBook HM1 (2)/ViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLightLabel: UIView!
+    @IBOutlet weak var yellowLightLabel: UIView!
+    @IBOutlet weak var greenLightLabel: UIView!
+    @IBOutlet weak var changeLightButton: UIButton!
+    
+    
+    let lightOn: CGFloat = 1
+    let lightOff: CGFloat = 0.2
+    
+    var count = 0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        lightConfigure()
+        
+        
+        
     }
-
-
+    
+    
+    
+    @IBAction func changeActionButton(_ sender: UIButton) {
+        
+        
+        switch count {
+        case 0:
+            redLightLabel.alpha = lightOn
+            yellowLightLabel.alpha = lightOff
+            greenLightLabel.alpha = lightOff
+            count = 1
+        case 1:
+            redLightLabel.alpha = lightOff
+            yellowLightLabel.alpha = lightOn
+            greenLightLabel.alpha = lightOff
+            count = 2
+        case 2:
+            redLightLabel.alpha = lightOff
+            yellowLightLabel.alpha = lightOff
+            greenLightLabel.alpha = lightOn
+            count = 0
+        default:
+            break
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    func lightConfigure() {
+        
+        redLightLabel.layer.cornerRadius = 75
+        yellowLightLabel.layer.cornerRadius = 75
+        greenLightLabel.layer.cornerRadius = 75
+        changeLightButton.layer.cornerRadius = 15
+        
+        redLightLabel.alpha = 0.2
+        yellowLightLabel.alpha = 0.2
+        greenLightLabel.alpha = 0.2
+        
+        view.backgroundColor = .black
+        
+    }
+    
+ 
+    
+    
 }
 
